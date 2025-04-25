f2053a646c8ee2a8c1d9d3114d56a4233fac23a7
LENOVO <LENOVO@DESKTOP-IH0009J>
2025-04-24 21:40:35 -0500

    1er primer commit (punto de guardado)

---
+++ GARAGE/GARAGE/GARAGE/src/main/java/com/mycompany/garage/Main.java
@@ -0,0 +1,14 @@
+package com.mycompany.garage;
+
+/**
+ *
+ * @author PCB
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ControlAcceso m = new ControlAcceso();
+        m.setVisible(true);
+        
+    }
+}
