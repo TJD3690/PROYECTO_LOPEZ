d2685c96d42473e6839b6c853a1f3be26216a268
LENOVO <LENOVO@DESKTOP-IH0009J>
2025-04-30 16:17:00 -0500

    2do commit(agrego de ventanas)

--- GARAGE/GARAGE/GARAGE/src/main/java/com/mycompany/garage/Main.java
+++ GARAGE/GARAGE/GARAGE/src/main/java/com/mycompany/garage/Main.java
@@ -1,13 +1,10 @@
 package com.mycompany.garage;
 
-/**
- *
- * @author PCB
- */
 public class Main {
 
     public static void main(String[] args) {
         ControlAcceso m = new ControlAcceso();
+        
         m.setVisible(true);
         
     }
